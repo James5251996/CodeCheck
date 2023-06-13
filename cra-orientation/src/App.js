ceb23d9c84eee4cfbe0f4f92df86a31e32e3a7dd
Trent Osterman <trentosterman@gmail.com>
2023-06-13 11:31:29 -0500

    added the logos for framework selection and lables for the logos as well.

--- cra-orientation/src/App.js
+++ cra-orientation/src/App.js
@@ -1,16 +1,19 @@
 
+import { Fragment } from 'react';
 import './App.css';
+import Framework from './Framework/Framework';
 
 function App() {
-  return (
+  return (<>
     <div className="App">
       <h1>CodeChecker</h1>
       <p>This is where framework type componnent will be</p>
-      <p>This is where difficulty component will be</p>
-      {/* this will be a component that will toggle with the solution to the problem given. */}
-      <p>Here i will have a text box for the question to be displayed</p>
+      <Framework/>
     </div>
-  );
+          <p>This is where difficulty component will be</p>
+          {/* this will be a component that will toggle with the solution to the problem given. */ }
+  <p>Here i will have a text box for the question to be displayed</p>
+  </>);
 }
 
 export default App;
---
+++ cra-orientation/src/Framework/Framework.js
@@ -0,0 +1,12 @@
+function Framework() {
+    return (<>
+        <div className="LogoSize">
+            <span><img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JavaScript"/>JavaScript</span>
+            <span><img src="https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp"/>React</span>
+            <span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"/>Python</span>
+            <span><img src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"/>C#</span>
+        </div>
+    </>)
+}
+
+export default Framework;
