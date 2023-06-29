e333889bc39da98450d5ece05a4a8785474fa36b
Trent Osterman <trentosterman@gmail.com>
2023-06-29 13:32:42 -0500

    created the login page and the login form

---
+++ cra-orientation/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const errors = useSelector(store => store.errors);
+    const dispatch = useDispatch();
+
+    const login = (event) => {
+        event.preventDefault();
+
+        if(username && password) {
+            dispatch({
+                type: 'LOGIN',
+                payload: {
+                    username: username,
+                    password: password,
+                },
+            })
+        } else {
+            dispatch({
+                type: 'LOGIN_INPUT_ERROR'
+            })
+        }
+    };
+
+    return (
+        <form onSubmit={login}>
+            <h2>Login</h2>
+            {errors.loginMessage && (
+                <h3 role='alert'>{errors.loginMessage}</h3>
+            )}
+            <div>
+                <label htmlFor="username">
+                    Username:
+                    <input
+                    type='text'
+                    name='username'
+                    required
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label htmlFor="password">
+                    Password:
+                    <input 
+                     type='password'
+                     name='password'
+                     required
+                     value={password}
+                     onChange={(event) => setPassword(event.target.value)}
+                     />
+                </label>
+            </div>
+            <div>
+                <input className="btn" type='submit' name='submit' value='Log In'></input>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm;
---
+++ cra-orientation/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+// here i need to import the login form. 
+import { useHistory } from 'react-router-dom';
+
+function LoginPage() {
+    const history = useHistory();
+
+    \return (
+        <div>
+            {/* log in form.  */}
+
+            <center>
+                <button
+                    type='button'
+                    className="btn btn_asLink"
+                    onClick={() => {
+                        history.push('/registration')
+                    }}>
+                    Register
+                </button>
+            </center>
+        </div>
+    )
+}
+
+export default LoginPage;
