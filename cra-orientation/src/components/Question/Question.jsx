63ee5d6b6f167d650b5c0495bbffb42c11b7cd55
Trent Osterman <trentosterman@gmail.com>
2023-08-09 10:41:17 -0500

    worked on the question component for where questions will be displayed.

--- cra-orientation/src/components/Question/Question.jsx
+++ cra-orientation/src/components/Question/Question.jsx
@@ -1,7 +1,8 @@
 function Question() {
-    return (
+    return (<>
         <p>the questions will be on a card you can flip</p>
-    )
+        <textarea>Here is yhe text are for the question. This will not ne editable.</textarea>
+        </> )
 }
 
 export default Question;
